ea5e1dc15d4dae38acc7064946dbb66fbe388798
HaroonJ <haroon.ansari123@gmail.com>
2020-07-07 15:51:35 +0530

    updated

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import {HttpClient } from '@angular/common/http';
+import { EmployeeModule } from './employee/employee.module';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: EmployeeModule
 //providedIn: 'root' ::used when you wants  to share  single  instance across 1 app.
-//providedIn: 'any' ::used when you wants to have single instance for  eagerly loaded component.
+//providedIn: 'any' ::used when you wants to have single instance for all eagerly loaded component.
 })
 export class LoginService {
 
